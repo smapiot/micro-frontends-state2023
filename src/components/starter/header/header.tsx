9a315ebab4f91b0b7ee18f8cbfe6932ca6d0d0bb
Florian Rappl <florian.rappl@smapiot.com>
2023-07-10 22:51:32 +0200

    Simplified

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -13,27 +13,8 @@ export default component$(() => {
         </div>
         <ul>
           <li>
-            <a
-              href="https://qwik.builder.io/docs/components/overview/"
-              target="_blank"
-            >
-              Docs
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://qwik.builder.io/examples/introduction/hello-world/"
-              target="_blank"
-            >
-              Examples
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://qwik.builder.io/tutorial/welcome/overview/"
-              target="_blank"
-            >
-              Tutorials
+            <a href="/logout">
+              Logout
             </a>
           </li>
         </ul>
