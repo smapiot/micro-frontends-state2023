e652d7ae9b1b79bd15337ca465dd0d4652c30b14
Florian Rappl <mail@florian-rappl.de>
2023-07-10 15:51:18 +0200

    Improved transform with header fallback

--- adapters/swa-transform.js
+++ adapters/swa-transform.js
@@ -1,21 +1,28 @@
 const { build } = require("esbuild");
 const { resolve } = require("path");
-const { rmSync, readdirSync, writeFileSync } = require("fs");
+const { rmSync, readdirSync, readFileSync, writeFileSync } = require("fs");
 
 const fnFn = "function.json";
 const targetFn = "render.js";
 
 const targetDir = resolve(__dirname, "../azure-functions/render");
 const fnJson = resolve(targetDir, fnFn);
+const outFile = resolve(targetDir, targetFn);
 const fnContent = require(fnJson);
-const allowedFiles = ['build', fnFn, targetFn];
+const allowedFiles = ["build", fnFn, targetFn];
 const scriptFile = fnContent.scriptFile;
+
+function replace(original, replacement) {
+  const content = readFileSync(outFile, "utf8");
+  const updated = content.replace(original, replacement);
+  writeFileSync(outFile, updated, "utf8");
+}
 
 build({
   entryPoints: [resolve(targetDir, scriptFile)],
   format: "cjs",
   bundle: true,
-  outfile: resolve(targetDir, targetFn),
+  outfile,
 }).then(
   (result) => {
     if (result.warnings.length) {
@@ -36,8 +43,15 @@ build({
     }
 
     fnContent.scriptFile = targetFn;
-    fnContent.entryPoint = 'default';
-    writeFileSync(fnJson, JSON.stringify(fnContent, undefined, 2), 'utf8');
+    fnContent.entryPoint = "default";
+    writeFileSync(fnJson, JSON.stringify(fnContent, undefined, 2), "utf8");
+
+    replace(
+      '.headers["x-ms-original-url"])',
+      '.headers["x-ms-original-url"] || "https://state-2023.microfrontends.cloud/")'
+    );
+
+    console.log('All modifications done!');
   },
   (err) => {
     console.error("Something bad happened", err);
